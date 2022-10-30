23461941cd6a3e15c804d280dc7ef747714318d5
nachen98 <nachen98@gmail.com>
2022-10-30 09:41:48 -0700

    added Email validators to login and sign up forms

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -24,6 +24,6 @@ def password_matches(form, field):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
     password = StringField('password', validators=[
                            DataRequired(), password_matches])
