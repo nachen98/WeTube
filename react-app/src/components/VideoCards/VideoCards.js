2d4eeea7e84ab8fa3a423ea6a6d6a7f07e758538
nachen98 <nachen98@gmail.com>
2022-11-17 12:23:27 -0800

    added thumbnail picture to each video

--- react-app/src/components/VideoCards/VideoCards.js
+++ react-app/src/components/VideoCards/VideoCards.js
@@ -1,21 +1,25 @@
 import "./VideoCards.css"
+import React from "react"
 import { Link } from "react-router-dom"
 import ReactPlayer from "react-player"
-export default function VideoCards({video}){
-    return (
-        <div className='link-for-video'>
-            <Link to={`/videos/${video.id}`} >
-                <div className="player-wrapper">
-                    <ReactPlayer
-                        className="react-player"
-                        url={video.body}
-                        width="100%"
-                        height="100%"
-                        controls={true}
-                />
 
-                </div>
-            </Link>
+export default function VideoCards({ video }) {
+    const playerRef=React.useRef(null)
+    return (
+        <div className="player-wrapper">
+            <ReactPlayer
+                className="react-player"
+                ref={playerRef}
+                url={video.body}
+                light={video.thumbnail_pic}
+                width="100%"
+                height="100%"
+                controls={true}
+            />
+        <div className='link-for-video'>
+            <Link to={`/videos/${video.id}`}></Link>
         </div>
+        </div>
+
     )
 }
