884234b90d352bf6579b2d140a1bd3490c170aa7
nachen98 <nachen98@gmail.com>
2022-11-24 11:15:24 -0800

    implemented the function to check filename format

--- app/api/aws.py
+++ app/api/aws.py
@@ -1,5 +1,19 @@
-import logging
 import boto3
-from botocore.exceptions import ClientError
+import botocore
 import os
+import uuid
 
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION=os.environ.get("AWS_DOMAIN")
+ALLOWED_EXTENSIONS={"pdf", "png", "jpg", "jpeg", "gif", "mp4", "mkv"}
+
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=os.environ.get("S3_KEY"),
+   aws_secret_access_key=os.environ.get("S3_SECRET")
+)
+
+#check if the filename is in allowed format
+def allowed_file(filename):
+    return "." in filename and \
+        filename.rsplit(".", 1)[1].lower in ALLOWED_EXTENSIONS
