0ade28357d675bbdd1276083e2541f294cb5633f
nachen98 <nachen98@gmail.com>
2022-11-24 11:27:15 -0800

    implemented function to get unique filename

--- app/api/aws.py
+++ app/api/aws.py
@@ -17,3 +17,8 @@ s3 = boto3.client(
 def allowed_file(filename):
     return "." in filename and \
         filename.rsplit(".", 1)[1].lower in ALLOWED_EXTENSIONS
+
+def get_unique_filename(filename):
+    ext=filename.rsplit(".", 1)[1].lower()
+    unique_filename=uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
