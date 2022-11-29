9a120fa9bbc410a2bde866105b00c1e68d35c9ec
nachen98 <nachen98@gmail.com>
2022-11-29 14:10:47 -0800

    edited signup code to dynamically render error messages

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -4,8 +4,10 @@ import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { signUp } from '../../store/session';
 import logo from "../Images/WeTube-logo.png"
+import "./SignUpForm.css"
+
 const SignUpForm = () => {
-  
+
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
@@ -15,110 +17,114 @@ const SignUpForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
-  
   const [errors, setErrors] = useState([]);
-  const [firstNameErrors, setFirstNameErrors] = useState("")
-  const [lastNameErrors, setLastNameErrors] = useState("")
-  const [emailErrors, setEmailErrors] = useState("")
-  const [userNameErrors, setUserNameErrors] = useState("")
-  const [passwordErrors, setPasswordErrors] = useState("")
-  const [repeatPasswordErrors, setRepeatPasswordErrors] = useState("")
+  const [hasSubmitted, setHasSubmitted] = useState(false);
 
-  useEffect(()=> {
-    if(!firstNameErrors) return;
-    setFirstNameErrors("")
-  }, [firstName])
+  useEffect(() => {
+    if(!hasSubmitted) return;
 
-  useEffect(()=> {
-    if(!lastNameErrors) return;
-    setLastNameErrors("")
-  }, [lastName])
+    let validationErrors = [];
 
-  useEffect(()=> {
-    if(!emailErrors) return;
-    setEmailErrors("")
-  }, [firstName])
-
-  useEffect(()=> {
-    if (!userNameErrors) return;
-    setUserNameErrors("")
-  }, [username])
-
-  useEffect(()=>{
-    if (!passwordErrors) return;
-    setPasswordErrors("")
-  }, [password])
-
-  useEffect(()=>{
-    if (!repeatPasswordErrors) return;
-    setRepeatPasswordErrors("")
-  }, [repeatPassword])
-
-  const onSignUp = async (e) => {
-    e.preventDefault();
-    const validationErrors = [];
-
-    if(firstName.trim().length > 40){
+    if (firstName.trim().length > 40) {
       validationErrors.push("The first name is limited to 40 characters.")
-      setFirstNameErrors("The first name is limited to 40 characters.")
     }
 
-    if(firstName.trim().length === 0) {
-      validationErrors.push("First name cannot be empty spaces.")
-      setFirstNameErrors("First name cannot be empty spaces.")
+    if (firstName.trim().length === 0) {
+        validationErrors.push("First name cannot be empty spaces.")
     }
 
-    if(lastName.trim().length > 40){
+    if (lastName.trim().length > 40) {
       validationErrors.push("The last name is limited to 40 characters.")
-      setLastNameErrors("The last name is limited to 40 characters.")
     }
 
-    if(lastName.trim().length === 0) {
+    if (lastName.trim().length === 0) {
       validationErrors.push("Last name cannot be empty spaces.")
-      setFirstNameErrors("Last name cannot be empty spaces.")
     }
 
-    if(username.length < 6){
+    if (username.length < 6) {
       validationErrors.push("Username must be at least 6 characters.")
-      setUserNameErrors("Username must be at least 6 characters.")
+      
     }
-    
-    if(username.length > 40){
+   
+    if (username.length > 40) {
       validationErrors.push("Username is limit to 40 characters.")
-      setUserNameErrors("Username is limit to 40 characters.")
     }
 
     if(password.length < 6){
-      validationErrors.push("Password must be at least 6 characters.")
-      setPasswordErrors("Password must be at least 6 characters.")
+      validationErrors.push("Password needs to be more than 6 characters.")
     }
 
-    if(password.length > 255){
+    if (password.length > 255) {
       validationErrors.push("Password is limit to 255 characters.")
-      setPasswordErrors("Password is limit to 255 characters.")
+    }
+
+    if (password !== repeatPassword) {
+      validationErrors.push("Password does not match.")
+    }
+
+    setErrors(validationErrors)
+
+  }, [firstName, lastName, email, username, password, repeatPassword])
+ 
+
+  const onSignUp = async (e) => {
+    if (!hasSubmitted) setHasSubmitted(true);
+
+    e.preventDefault();
+    let validationErrors = [];
+
+    if (firstName.trim().length > 40) {
+      validationErrors.push("The first name is limited to 40 characters.")
+    }
+
+    if (firstName.trim().length === 0) {
+      validationErrors.push("First name cannot be empty spaces.")
+    }
+
+    if (lastName.trim().length > 40) {
+      validationErrors.push("The last name is limited to 40 characters.")
+    }
+
+    if (lastName.trim().length === 0) {
+      validationErrors.push("Last name cannot be empty spaces.")
+    }
+
+    if (username.length < 6) {
+      validationErrors.push("Username must be at least 6 characters.")
+    }
+
+    if (username.length > 40) {
+      validationErrors.push("Username is limit to 40 characters.")
+    }
+
+    if (password.length < 6) {
+      validationErrors.push("Password must be at least 6 characters.")
+    }
+
+    if (password.length > 255) {
+      validationErrors.push("Password is limit to 255 characters.")
     }
 
     if (password !== repeatPassword) {
       validationErrors.push("Passwords do not match.")
-      setRepeatPasswordErrors("Passwords do not match.")
     }
 
-    if(validationErrors.length > 0) return setErrors(validationErrors)
+    if (validationErrors.length > 0) return setErrors(validationErrors)
 
     const data = await dispatch(signUp(firstName, lastName, username, email, password));
-   
+
     if (data) {
       const formatedErrors = data.map(err => {
         const errMsgArr = err.split(":");
-        return errMsgArr.length >1 ? errMsgArr.slice(1) : errMsgArr
-        })
+        return errMsgArr.length > 1 ? errMsgArr.slice(1) : errMsgArr
+      })
       setErrors(formatedErrors)
       return
-      }
-      return <Redirect to ="/" />
     }
-  
-  
+    return <Redirect to="/" />
+  }
+
+
 
   const updateFirstName = (e) => {
     setFirstName(e.target.value)
@@ -151,91 +157,94 @@ const SignUpForm = () => {
   return (
     <div id="signup-container">
 
-      <div id="signup-box">
-        <div id="signup-inner-container">
-        <div className='flx-col-justify-algn-ctr'>
-            <img src={logo} id="home-logo" alt="logo" />
+      <div id="signup-box" className="flx-col-justify-align-ctr">
+
+
+        <img src={logo} id="home-logo" alt="logo" />
+
+        <div id="create-an-account" className='flx-col-justify-algn-ctr'>Create your WeTube Account</div>
+        <form id="signup-form" onSubmit={onSignUp}>
+          <div id="error-messages">
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
           </div>
-          <div id="create-an-account" className='flx-col-justify-algn-ctr'>Create your WeTube Account</div>
-          <form onSubmit={onSignUp}>
-            <div id="error-messages">
-              {errors.map((error, ind) => (
-                <div key={ind}>{error}</div>
-              ))}
-            </div>
 
+          <div className="justify-space-btw">
             <div>
-              <label className="custom-field">
-                <input className='login-input'
-                  type='text'
-                  value={firstName}
-                  onChange={updateFirstName}
-                  required
-                />
-                <span className="placeholder">{!firstName.length? "First name": ""}</span>
-              </label>
+            <label className="custom-field">
+              <input id="first-name-input" className='signup-input'
+                type='text'
+                value={firstName}
+                onChange={updateFirstName}
+                required
+              />
+              <span className="placeholder">{!firstName.length ? "First name" : ""}</span>
+            </label>
             </div>
             <div>
-              <label className="custom-field">
-                <input className='login-input'
-                  type='text'
-                  value={lastName}
-                  onChange={updateLastName}
-                  required
-                />
-                <span className="placeholder">{!lastName.length? "Last name": ""}</span>
-              </label>
+            <label className="custom-field">
+              <input className='signup-input'
+                type='text'
+                value={lastName}
+                onChange={updateLastName}
+                required
+              />
+              <span className="placeholder">{!lastName.length ? "Last name" : ""}</span>
+            </label>
             </div>
+          </div>
+          <div>
+            <label className="custom-field">
+              <input className='email-username-sign-up-input signup-input'
+                type='email'
+                value={email}
+                onChange={updateEmail}
+                required
+              />
+              <span className="placeholder">{!email.length ? "Your email address" : ""}</span>
+            </label>
+          </div>
 
-            <div>
-              <label className="custom-field">
-                <input className='login-input'
-                  type='email'
-                  value={email}
-                  onChange={updateEmail}
-                  required
-                />
-                <span className="placeholder">{!email.length? "Your email address": ""}</span>
-              </label>
-            </div>
-
-            <div>
-              <label className="custom-field">
-                <input className='login-input'
-                  type='text'
-                  value={username}
-                  onChange={updateUsername}
-                  required
-                />
-                <span className="placeholder">{!username.length? "Your username": ""}</span>
-              </label>
-            </div>
-            <div>
-              <label className="custom-field">
-                <input className='login-input'
-                  type='text'
-                  value={password}
-                  onChange={updatePassword}
-                  required
-                />
-                <span className="placeholder">{!password.length? "Password": ""}</span>
-              </label>
-            </div>
-            <div>
-              <label className="custom-field">
-                <input className='login-input'
-                  type='text'
-                  value={repeatPassword}
-                  onChange={updateRepeatPassword}
-                  required
-                />
-                <span className="placeholder">{!password.length? "Confirm": ""}</span>
-              </label>
-            </div>
-            <Link to={"/login"} id="linktologin">Sign in instead</Link>
-            <button type='submit' id="signup-button">Sign Up</button>
-          </form>
+          <div>
+            <label className="custom-field">
+              <input className='email-username-sign-up-input signup-input'
+                type='text'
+                value={username}
+                onChange={updateUsername}
+                required
+              />
+              <span className="placeholder">{!username.length ? "Your username" : ""}</span>
+            </label>
+          </div>
+          <div className="justify-space-btw">
+          <div>
+            <label className="custom-field">
+              <input className='signup-input'
+                type='text'
+                value={password}
+                onChange={updatePassword}
+                required
+              />
+              <span className="placeholder">{!password.length ? "Password" : ""}</span>
+            </label>
+          </div>
+          <div>
+            <label className="custom-field">
+              <input className='signup-input'
+                type='text'
+                value={repeatPassword}
+                onChange={updateRepeatPassword}
+                required
+              />
+              <span className="placeholder">{!password.length ? "Confirm" : ""}</span>
+            </label>
+          </div>
         </div>
+          <Link to={"/login"} id="linktologin">Sign in instead</Link>
+          <button type='submit' id="signup-button">Sign Up</button>
+        </form>
+
       </div>
     </div>
   );
