709a9a55763e8c2c3e4b7122decdeeb7d1058f24
nachen98 <nachen98@gmail.com>
2022-11-30 19:15:20 -0800

    implemented footer

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import "./Footer.css";
+
+function Footer() {
+
+    return (
+        <>
+
+            <div className="footer-container flx-row-space-btw">
+
+                <div className="wetube-designer">
+                    WeTube is designed by Na Chen
+                </div>
+                <div className="credential-links flx-row">
+                    <div >
+               
+                            <button class="fa-brands fa-github"></button>
+                        
+                    </div>
+
+                    <div >
+            
+                            <button class="fa-brands fa-linkedin"></button>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
