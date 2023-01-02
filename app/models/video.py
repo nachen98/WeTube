9a7193fef637a413d910d0f832d9e1163b5dfc73
nachen98 <nachen98@gmail.com>
2023-01-02 10:56:03 -0800

    implemented the videolikes modle

--- app/models/video.py
+++ app/models/video.py
@@ -15,7 +15,7 @@ class Video(db.Model):
 
     user = db.relationship("User", back_populates="videos")
     comments = db.relationship("Comment", back_populates="video", cascade="all, delete-orphan")
-   
+    videolikes = db.relationship("VideoLikes", back_populates="video", cascade="all, delete-orphan")
     
     def to_dict(self):
         return {
---
+++ app/models/videolikes.py
@@ -0,0 +1,24 @@
+from .db import db
+
+class VideoLikes(db.Model):
+    __tablename__='videolikes'
+
+    id=db.Column(db.Integer, primary_key=True)
+    is_like=db.Column(db.Boolean, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    video_id = db.Column(db.Integer, db.ForeignKey("videos.id"), nullable=False)
+
+    user = db.relationship("User", back_populates="videolikes")
+    video = db.relationship("Video", back_populates="videolikes")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'is_like': self.is_like,
+            'user_id': self.user_id,
+            'video_id': self.video_id,
+        }
+
+    def __repr__(self):
+        return f'<VideoLikes, id={self.id}, is_like={self.is_like}>'
