117a961ae6cdc270128bc59db2b3accf0169b38b
nachen98 <nachen98@gmail.com>
2023-01-15 11:10:46 -0800

    styled videolikes dislike button

--- react-app/src/components/VideoLikeDislike/VideoLikeDislike.js
+++ react-app/src/components/VideoLikeDislike/VideoLikeDislike.js
@@ -13,13 +13,13 @@ export function VideoLikeDislike({ videoId }) {
     const video = videos[videoId]
     const [numLikes, setNumLikes] = useState(video.video_likes)
 
-    const currUser = useSelector(state=>state.session.user)
-    
+    const currUser = useSelector(state => state.session.user)
+
     let [like, setLike] = useState(undefined)
     const [thumbUpClick, setThumbUpClick] = useState('fa-regular fa-thumbs-up fa-xl')
     const [thumbDownClick, setThumbDownClick] = useState('fa-regular fa-thumbs-down fa-xl')
 
-    
+
     console.log("############################# like", like)
 
 
@@ -29,26 +29,26 @@ export function VideoLikeDislike({ videoId }) {
     useEffect(async () => {
         setLike(undefined)
         setNumLikes(video.video_likes)
-        if(currUser === null)
+        if (currUser === null)
             return
 
         //res in the .then() is the same as the allLikes in the thunk of getAllLikes
         await dispatch(getAllLikes(videoId))
-        .then((res)=>{
-            const _like = res[`${video.id}-${currUser.id}`]?.is_like
+            .then((res) => {
+                const _like = res[`${video.id}-${currUser.id}`]?.is_like
 
-            setLike(_like===undefined?0:_like)
-            setThumbUpClick(_like===1? 'fa-solid fa-thumbs-up fa-xl':'fa-regular fa-thumbs-up fa-xl')
-            setThumbDownClick(_like===-1?'fa-solid fa-thumbs-down fa-xl':'fa-regular fa-thumbs-down fa-xl')
-        })
+                setLike(_like === undefined ? 0 : _like)
+                setThumbUpClick(_like === 1 ? 'fa-solid fa-thumbs-up fa-xl' : 'fa-regular fa-thumbs-up fa-xl')
+                setThumbDownClick(_like === -1 ? 'fa-solid fa-thumbs-down fa-xl' : 'fa-regular fa-thumbs-down fa-xl')
+            })
     }, [videoId])
 
 
     const update = async (like_) => {
-    console.log("$$$$$$$$$$$$$ like use effect", like_)
+        console.log("$$$$$$$$$$$$$ like use effect", like_)
         await dispatch(postVideoLike(videoId, like_))
-        setThumbUpClick(like_===1? 'fa-solid fa-thumbs-up fa-xl':'fa-regular fa-thumbs-up fa-xl')
-        setThumbDownClick(like_===-1?'fa-solid fa-thumbs-down fa-xl':'fa-regular fa-thumbs-down fa-xl')
+        setThumbUpClick(like_ === 1 ? 'fa-solid fa-thumbs-up fa-xl' : 'fa-regular fa-thumbs-up fa-xl')
+        setThumbDownClick(like_ === -1 ? 'fa-solid fa-thumbs-down fa-xl' : 'fa-regular fa-thumbs-down fa-xl')
     }
 
 
@@ -56,81 +56,78 @@ export function VideoLikeDislike({ videoId }) {
     //then call the update function which triger dispatch to post the new like, 
     const likeButton = async () => {
         console.log("--------------------- like", like)
-        if(!currUser) 
+        if (!currUser)
             alert('Like this video? Please sign in to make your opinion count.')
         else if (like === 1) {
             setLike(0)
             update(0)
             setThumbUpClick('fa-regular fa-thumbs-up fa-xl')
 
-            
-            setNumLikes(numLikes-1)
-       
-        }else if (like === -1){
+
+            setNumLikes(numLikes - 1)
+
+        } else if (like === -1) {
             setLike(1)
             update(1)
             setThumbUpClick('fa-solid fa-thumbs-up fa-xl')
             setThumbDownClick('fa-regular fa-thumbs-down fa-xl')
 
-            setNumLikes(numLikes+1)
-            
+            setNumLikes(numLikes + 1)
+
         }
 
-         else if(like ===0){
+        else if (like === 0) {
             setLike(1)
             update(1)
             setThumbUpClick('fa-solid fa-thumbs-up fa-xl')
-            setNumLikes(numLikes+1)
+            setNumLikes(numLikes + 1)
             // if (thumbDownClick === 'fa-solid fa-thumbs-down fa-xl') setThumbDownClick('fa-regular fa-thumbs-down fa-xl')
         }
     }
     const dislikeButton = async () => {
-        
-        if(!currUser) alert('Dislike this video? Please sign in to make your opinion count.')
+
+        if (!currUser) alert('Dislike this video? Please sign in to make your opinion count.')
         else if (like === 0) {
             setLike(-1)
             update(-1)
             setThumbDownClick('fa-solid fa-thumbs-down fa-xl')
-        
 
-        } else if(like === 1) {
+
+        } else if (like === 1) {
             setLike(-1)
             update(-1)
             setThumbDownClick('fa-solid fa-thumbs-down fa-xl')
             setThumbUpClick('fa-regular fa-thumbs-up fa-xl')
-    
-            setNumLikes(numLikes-1)
+
+            setNumLikes(numLikes - 1)
         }
-        
-        
+
+
         else if (like === -1) {
             setLike(0)
             update(0)
             setThumbDownClick('fa-regular fa-thumbs-down fa-xl')
-         
+
         }
     }
 
 
     return (
         <>
-            <div id="like-button"
-                onClick={()=>
-                    {
-                        console.log("~~~~~~~~~~~~~~~ like", like)
-                        likeButton()
-                    }}
-            >
-                <i className={thumbUpClick}></i>
+            <div id="like-button" onClick={likeButton}>
+
+                <i className={thumbUpClick} id="thumb-up-icon"></i>
+
+                <span id="likes-count">{numLikes}</span>
             </div>
-            <span>{numLikes}</span>
-            <span id='likes-dislikes-spacer'>|</span>
-            <div id="dislike-button"
-                onClick={dislikeButton}
-            >
-                <i className={thumbDownClick}></i>
+
+            <div id='likes-dislikes-spacer'></div>
+            <div id="dislike-button" onClick={dislikeButton}>
+               
+                    <i className={thumbDownClick} id="thumb-down-icon"></i>
 
             </div>
+
 
         </>
 
